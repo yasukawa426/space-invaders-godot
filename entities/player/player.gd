7697ad6103b40c589b36aa85cf468c8fd6ac6664
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-05 20:14:57 -0300

    feat/refact: move direction to unhandled_input and add rotation animation to movement

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,27 +1,45 @@
 extends CharacterBody2D
-const SPEED = 150.0
-const JUMP_VELOCITY = -400.0
+## The maximum speed the player can move at.
+const MAX_SPEED: float = 150.0
+## The acceleration of the player when moving.
+const ACCELERATION: float = 20000
+
+## Amount of degrees the player will rotate when moving left or right.
+const _TURN_DEGREE: float = 10.0
 
 @onready var _screen_size: Vector2 = get_viewport_rect().size
 @onready var _sprite_width: int = $Sprite2D.texture.get_width()
 ## Bullet that will be spawned when shooting. Should be a scene that extends Bullet.
 @export var bullet: PackedScene;
 
+## Direction the player is currently moving towards. -1 for left, 1 for right, 0 for no movement.
+var _direction: float = 0.0
+
 ## Wether the player can shoot. Will only be true if there is no existing missile. 
 var _can_fire: bool = true
 
-func _process(delta: float) -> void:
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+
+
+func _physics_process(delta: float) -> void:
+	if _direction:
+		velocity.x = move_toward(velocity.x, MAX_SPEED * _direction, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION  * delta * 2)
 
 	move_and_slide()
+
+	var tween: Tween = create_tween()
+	tween.tween_property($Sprite2D, "rotation", deg_to_rad(_direction * _TURN_DEGREE), 0.1)
 	
-	position = position.clamp(Vector2(0 + (_sprite_width / 2), position.y), Vector2(_screen_size.x - (_sprite_width / 2), position.y))
+
+	print("rotation: ", rad_to_deg($Sprite2D.rotation))
+	
+	## Clamp the player's position to the screen bounds considering the sprite's width.
+	position = position.clamp(Vector2(0 + (_sprite_width / 2.0), position.y), Vector2(_screen_size.x - (_sprite_width / 2.0), position.y))
 
 func _unhandled_input(event: InputEvent) -> void:
+	_direction = Input.get_axis("move_left", "move_right")
+
 	if event.is_action_pressed("shoot") and _can_fire:
 		_shoot()
 
