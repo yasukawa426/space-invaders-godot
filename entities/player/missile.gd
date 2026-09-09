34be148f5c4e2d6ed60d41f2f5137f7c241811c0
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-09 13:52:22 -0300

    feat: destroy bullet when hitting enemy

--- entities/player/missile.gd
+++ entities/player/missile.gd
@@ -1,1 +1,8 @@
 extends Bullet
+
+func _on_hit_something(area: Area2D):
+	if area is Enemy:
+		print("I (the bullet) just hit an enemy")
+		_destroy()
+	else: 
+		print("I (the bullet) hit something else")
