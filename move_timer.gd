f64e6099f7bb629fd9968682bac8c0470f8a65b1
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-10 19:02:45 -0300

    feat: move timer only starts counting when formation is ready and pauses for a short delay on enemy death.

--- move_timer.gd
+++ move_timer.gd
@@ -2,19 +2,20 @@ extends Timer
 
 const MIN_WAIT_TIME: float = 0.01
 const MAX_WAIT_TIME: float = 0.7
+const DEATH_PAUSE_AMOUNT: float = 0.2
 
-func _ready() -> void:
-	start(MAX_WAIT_TIME)
-
-## Decreases await time when enemy dies.
+## Decreases await time when enemy dies and stops movement for a moment.
 func _on_grid_formation_enemy_died(score: int) -> void:
 	var alive: int =  $"../Entities/GridFormation".alive_enemies 
 	var total: int =  $"../Entities/GridFormation".total_enemies
-	
 	var progress: float = float(alive) / float(total)
+	
+	paused = true
+	await get_tree().create_timer(DEATH_PAUSE_AMOUNT).timeout
+	paused = false
 	# Gets exponentially faster the less enemy there is
 	var new_wait_time = lerp(MIN_WAIT_TIME, MAX_WAIT_TIME, progress ** 1.2)
 	
-	print("alive: ", alive, "total: ", total, "progress: ",  progress)
+	print("alive: ", alive, "total: ", total, "progress: ",  progress ** 2)
 	wait_time = new_wait_time
 	print(new_wait_time)
