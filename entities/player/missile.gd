835afefa69c81add7807d85c15158b4d3bf20e4f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-21 13:07:38 -0300

    feat: add bullet hitbox body check for another bullet

--- entities/player/missile.gd
+++ entities/player/missile.gd
@@ -4,5 +4,7 @@ func _on_hit_something(area: Area2D):
 	if area is Enemy:
 		print("I (the bullet) just hit an enemy")
 		_destroy()
+	elif area is Bullet:
+		print("I (the bullet) just hit a bullet")
 	else: 
 		print("I (the bullet) hit something else")
