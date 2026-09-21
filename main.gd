c7cd0aa274aa981a58f3c89ec6eec65ef85c9724
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-21 13:27:02 -0300

    feat: add visual scaling effect for earth and shield

--- main.gd
+++ main.gd
@@ -13,6 +13,13 @@ func _ready() -> void:
 	for entity in $Entities.get_children():
 		entity.scale = Vector2.ONE * ENTITIES_SCALING
 
+func _process(_delta: float) -> void:
+	# This is just a visual effect to make the earth and shield look a little more alive. It makes them scale up and down horizontally subtly
+	# We use the the amount of time passed since the game started multiplied by a veeery small number to get a increasing value and then use the sine of that value to scale them between 1.0 and 1.2.
+	var t := Time.get_ticks_msec() * 0.00004
+	var scale_value = 1.1 + sin(t) * 0.1
+	$Earth.scale.x = scale_value
+	$Shield.scale.x = scale_value
 
 ## Enemy died, increase score
 func _on_grid_formation_enemy_died(score: int) -> void:
