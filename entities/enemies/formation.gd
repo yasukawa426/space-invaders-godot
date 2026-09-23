8fef4fc15b33101be6242d3b8505596dccf4494f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-23 09:49:00 -0300

    feat: add formation enemy shooting.

--- entities/enemies/formation.gd
+++ entities/enemies/formation.gd
@@ -31,10 +31,21 @@ const _PACKED_ENEMY: PackedScene = preload("res://entities/enemies/enemy.tscn")
 const _INITIAL_AUDIO_PITCH_SCALE: float = 0.6
 ## The amount the pitch scale of the formation movement sound will decrease each time the sound is played.
 const _AUDIO_PITCH_SCALE_STEP: float = 0.1
-
+## The initial amount of time in seconds it takes for an enemy to charge before shooting a bullet. 
+const _INITIAL_ENEMY_CHARGE_TIME: float = 2.0
+## The initial amount of time in seconds between each different individual enemy starts charging. Slightly randomized to avoid enemies having a set rhythm.
+const _INITIAL_ENEMY_CHARGE_DELAY: float = 4.0
 
 ## AudioStreamPlayer for the formation movement.
 @onready var move_audio_player: AudioStreamPlayer = $"../FormationAudioStreamPlayer"
+## Timer for controlling the time between each different individual enemy starts charging.
+@onready var _shoot_timer: Timer = $"../ShootTimer"
+## The amount of time it takes for an enemy to charge before shooting a bullet. Decreases as enemies die.
+@onready var _enemy_charge_time: float = _INITIAL_ENEMY_CHARGE_TIME
+## The amount of time between each different individual enemy starts charging. Decreases as enemies die.
+@onready var _enemy_charge_delay: float = _INITIAL_ENEMY_CHARGE_DELAY
+## Scene to be instantieted as enemies bullet
+@export var _bullet_scene: PackedScene = preload("res://entities/enemies/laser.tscn")
 
 ## Number of total enemies this formation has.
 var _total_enemies: int
@@ -46,6 +57,8 @@ var _has_to_move_down: bool = false
 var _direction: int = 1 
 ## The initial position of the formation when it is created. Used to reset the formation to its original position.
 var _initial_position: Vector2
+
+# TODO: decrease shooting delay and shooting charging time to increase dificculty based on wave 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
@@ -91,15 +104,57 @@ func _on_enemy_died(score: int) -> void:
 	if _alive_enemies <= 0:
 		formation_destroyed.emit()
 
+## Called when an enemy has finished charging and should shoot a bullet. Spawns a bullet as a sibling of the formation.
+func _on_enemy_finished_charging(bullet_global_spawn_position: Vector2) -> void:
+	var projectile: Bullet = _bullet_scene.instantiate()
+	projectile.global_position = bullet_global_spawn_position
+	
+	add_sibling(projectile)
+
+## Called when the shoot timer times out. Tells a random enemy that has no other enemy below it to start charging to shoot a bullet, and then restart the timer with a new delay.
+func _on_shoot_timer_timeout() -> void:
+	var enemies: Array[Enemy] = []
+
+	for enemy in get_children():
+		if enemy is Enemy:
+			enemies.append(enemy)
+	
+	## list of enemies that are allowed to shoot.
+	var shooters: Array[Enemy] = []
+
+
+	for enemy in enemies:
+		var can_shoot: bool = true
+
+		for other_enemy in enemies:
+			# if there is a enemy directly bellow of it, its not allowed to shoot. go to the next one
+			if other_enemy.formation_position.y > enemy.formation_position.y and enemy.formation_position.x == other_enemy.formation_position.x:
+				can_shoot = false
+				break
+		
+		# if not, assign as a shooter
+		if can_shoot:
+			shooters.append(enemy)
+
+	# no one can shoot babe
+	if shooters.is_empty():
+		return
+
+	# picks a random shooter and tells them to start charging.
+	var random_enemy: Enemy = shooters.pick_random()
+	random_enemy.start_charging(_enemy_charge_time)
+
+	# starts the shoot timer again with a +-20% variation
+	_shoot_timer.start(_enemy_charge_delay * randf_range(0.8, 1.2))
 
 ## Called when the formation touches the left world border. Sets the direction to the right and _has_to_move_down to true.
-func _on_left_border_area_entered(area: Area2D) -> void:
+func _on_left_border_area_entered(_area: Area2D) -> void:
 	print("touched left border")
 	_direction = 1
 	_has_to_move_down = true
 
 ## Called when the formation touches the right world border. Sets the direction to the left and _has_to_move_down to true.
-func _on_right_border_area_entered(area: Area2D) -> void:
+func _on_right_border_area_entered(_area: Area2D) -> void:
 	print("touched right border")
 	_direction =  -1
 	_has_to_move_down = true
@@ -125,7 +180,6 @@ func _reset_formation(columns: int, rows: int) -> void:
 	for enemy in get_children():
 		enemy.queue_free()
 
-
 	for row in range(rows):
 		for column in range(columns):
 			var enemy: Enemy = _PACKED_ENEMY.instantiate()
@@ -143,7 +197,9 @@ func _reset_formation(columns: int, rows: int) -> void:
 				enemy.set_type(Enemy.Types.SQUARE)
 				
 			enemy.died.connect(_on_enemy_died)
+			enemy.finished_charging.connect(_on_enemy_finished_charging)
 
 			await get_tree().create_timer(enemy_spawn_delay).timeout
 	
+	_shoot_timer.start(_enemy_charge_delay)
 	formation_ready.emit(_total_enemies)
